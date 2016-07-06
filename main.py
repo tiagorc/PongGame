1e10e6ae0af6a95579391a8b14fa16166a30d94e
tiagorc <tiagobrasil2005@yahoo.com.br>
2016-07-06 14:17:01 -0300

    signals implemented

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from kivy.clock import Clock
 from random import randint
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.core.audio import SoundLoader
+from kivy.event import EventDispatcher
 
 
 
@@ -20,9 +21,22 @@ class PongBall(Widget):
         self.pos = Vector(*self.velocity) + self.pos
 
 
+class GameOverEventDispatcher(EventDispatcher):
+    def __init__(self, **kwargs):
+        self.register_event_type('on_game_over')
+        super(GameOverEventDispatcher, self).__init__(**kwargs)
+
+    def define_game_over(self, value):
+        # when define_game_over is called, the 'on_game_over' event will be
+        # dispatched with the value
+        self.dispatch('on_game_over', value)
+
+    def on_game_over(self, *args):
+        print 'entrou no dispatch do game over', args
+
+
 class PongGame(Widget):
     ball = ObjectProperty(None)
-
 
     def serve_ball(self, vel=(4,0)):
         self.ball.center = self.center
@@ -43,9 +57,13 @@ class PongGame(Widget):
         if self.ball.x < self.x:
             self.player2.score += 1
             self.serve_ball(vel=(4, 0))
+            if self.player2.score > 3:
+                self.game_over_callback(self.player2.score, 'Player 2 Wins!')
         if self.ball.x > self.width:
             self.player1.score += 1
             self.serve_ball(vel=(-4, 0))
+            if self.player1.score > 3:
+                self.game_over_callback(self.player1.score, 'Player 1 Wins!')
 
 
     def on_touch_move(self, touch):
@@ -55,13 +73,20 @@ class PongGame(Widget):
             self.player2.center_y = touch.y
 
 
+    def game_over_callback(value, *args):
+        """
+        calls event dispatcher to do something
+        """
+        ev_disp = GameOverEventDispatcher()
+        ev_disp.define_game_over(args)
+
 
 class PongApp(App):
     def build(self):
         sound = SoundLoader.load('resources/sound.mp3')
         if sound:
-            print("Sound found at %s" % sound.source)
-            print("Sound is %.3f seconds" % sound.length)
+            # print("Sound found at %s" % sound.source)
+            # print("Sound is %.3f seconds" % sound.length)
             sound.play()
 
     def set_state(self, state):
